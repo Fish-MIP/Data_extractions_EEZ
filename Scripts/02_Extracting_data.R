e55e70a35e9f85571a540cdc853ee66027cc0942
camillanovaglio <camilla.novaglio@gmail.com>
2023-11-20 20:47:40 +0000

    Solving issues - cami's editis

--- Scripts/02_Extracting_data.R
+++ Scripts/02_Extracting_data.R
@@ -17,37 +17,59 @@ library(tidyr)
 input_folder <- "/rd/gem/private/users/camillan/Extract_tcblog10_Data/Output/raster_annual_sumSize"
 
 #Getting list of biomass files
-input_files <- list.files(input_folder, full.names = T)
-
+### CN 
+input_files <- list.files(input_folder ,full.names = T) #pattern = "2023-11-16.+global",
 
 # Masks -------------------------------------------------------------------
 mask_all <- rast("Outputs/EEZMasks/EEZ_mask_1deg.nc")
-mask_dbpm <- rast("Outputs/EEZMasks/EEZ_mask_1deg_DBPM.nc")
-
+# mask_dbpm <- rast("Outputs/EEZMasks/EEZ_mask_1deg_DBPM.nc")
 
 # Grid cell area ----------------------------------------------------------
-area_all <- rast("ESM_Sample_Data/area_1deg.nc")
-area_dbpm <- rast("ESM_Sample_Data/area_1deg_DBPM.nc")
+# area_all <- rast("ESM_Sample_Data/area_1deg.nc")
+# area_dbpm <- rast("ESM_Sample_Data/area_1deg_DBPM.nc")
 
 
 # Define functions --------------------------------------------------------
 #Multiply raster by area and transform to terra
 weight_mask <- function(ras_multi, weight){
+  
+  # # trial CN
+  # ras_multi = brick
+  # weight = area
+  
   #Crop if extent is not the same
   if(ext(ras_multi) != ext(weight)){
     weight <- crop(weight, ext(ras_multi))
   }
   multi <- list()
   for(i in 1:dim(ras_multi)[3]){
-    ras <- rast(ras_multi[[i]])
+    
+    ## CN changed if this was already transformed into terra object 
+    # ras <- rast(ras_multi[[i]]), instead: 
+    ras<-ras_multi
     multi[[i]] <- ras*weight
   }
   multi_ras <- rast(multi)
+  
+  # ## CN or simply?? 
+  # trial<-ras_multi*weight
+  # plot(multi_ras)
+  # plot(trial)
+  # 
+  # plot(multi_ras[[50]])
+  # plot(trial[[50]])
+  # plot(weight)
+  
   return(multi_ras)
 }
 
 #Extract data by EEZ
 sum_bio <- function(ras, eez_mask, meta){
+  
+  # # CN trial 
+  # ras = weighted_terra # this is values * grid cell area 
+  # eez_mask = mask
+  
   #Create empty data frame to store results
   summaries_biomass <- data.frame()
   #Getting years from biomass raster
@@ -61,11 +83,21 @@ sum_bio <- function(ras, eez_mask, meta){
     pull()
   #Extract each EEZ and apply mask
   for(i in unique_id){
+    
+    # CN trial 
+    # i = unique_id[10]
+    
     #Get single EEZ
     eez <- eez_mask == i
     eez[eez == 0] <- NA
+    # CN: 
+    # terra::plot(eez)
+    
     #Apply EEZ mask
     weighted_eez <- ras*eez
+    ## CN 
+    # terra::plot(weighted_eez[[50]])
+    
     #Transform into tabular form
     weighted_eez <- as.data.frame(weighted_eez) 
     #Correct names in data frame - Use years
@@ -77,9 +109,11 @@ sum_bio <- function(ras, eez_mask, meta){
       group_by(year) |> 
       #Add all biomass values
       summarise(total_biomass_g_m2 = sum(biomass, na.rm = F),
+                #### CN should this be weighted.mean instead? I cannot understand where the /sumOfWeights part is...  
                 mean_weighted_biomass_G_m2 = mean(biomass, na.rm = F)) |> 
       #Add code for EEZ
       mutate(eez = i)
+    ## CN not sure what bind_rows does in this case - should it be outside the loop? 
     summaries_biomass <- summaries_biomass |> 
       bind_rows(w_e)
   }
@@ -102,25 +136,43 @@ if(dir.exists(output_folder) == F){
 
 # Applying functions to all files -----------------------------------------
 for(f in input_files){
+  
+  # CN trial
+  # problem: 
+  f = "/rd/gem/private/users/camillan/Extract_tcblog10_Data/Output/raster_annual_sumSize/2023-11-15_01_raster_annual_sumSize_apecosm_ipsl_historical_global.rds"
+  # OK - saved as terra instead of raster 
+  f = "/rd/gem/private/users/camillan/Extract_tcblog10_Data/Output/raster_annual_sumSize/2023-11-20_01_raster_annual_sumSize_zoomss_gfdl_historical_global.rds"
+  
   #Read file
   brick <- readRDS(f)
   #Turn into terra
   brick <- brick[[names(brick)]]
   
   #Locate correct mask and area
-  if(str_detect(f, "dbpm|zoomss_ipsl")){
-    mask <- mask_dbpm
-    area <- area_dbpm
-  }else{
+  
+  #### CN change this  
+  # if(str_detect(f, "dbpm|zoomss_ipsl")){
+    # mask <- mask_dbpm
+    # area <- area_dbpm
+  # }else{
     mask <- mask_all
-    area <- area_all
-  }
+    # area <- area_all
+    
+    # If rds are saved as raster object - though sometimes this is not working (with RasterLayer) 
+    # forArea<-as(brick, "SpatRaster")
+    # forArea<-rast(brick)
+    # area <-cellSize(forArea[[1]], unit = "m")
+    # If rds are saved as terra objects - though warning message when transforming from raster to terra in original code
+    area <-cellSize(brick[[1]], unit = "m")
+    terra::plot(area)
+  # }
   
   #Metadata to be added to file
   meta <- str_extract(f, ".*sumSize_(.*)", group = 1) |> 
     str_split(pattern = "_", simplify = T)
   
   #Weighting data
+  # CN this is values * gridcell area 
   weighted_terra <- weight_mask(brick, area)
   #Calculating summaries
   summaries <- sum_bio(weighted_terra, mask, meta)
